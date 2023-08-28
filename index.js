d60ebfd0684dfd7437fb12553ac319db39014cb4
CarlosMunozDiaz <carlos10md@gmail.com>
2023-08-28 12:10:52 +0200

    Random code in index

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Github Actions Test")
+var fs = require('fs');
+
+let randomData = [];
+
+for(let i = 0; i < 120; i++) {
+    randomData.push(parseInt(Math.random()*100));
+}
+
+let randomJson = JSON.stringify(randomData);
+let nameJson = 'random_' + Date.now();
+
+fs.writeFile(`./data/${nameJson}.json`, randomJson);
