5064ce2d105b38e79947f081c5b7a41e20f4738c
CarlosMunozDiaz <carlos10md@gmail.com>
2023-08-28 14:04:55 +0200

    Actualización github actions

--- index.js
+++ index.js
@@ -9,7 +9,4 @@ for(let i = 0; i < 120; i++) {
 let randomJson = JSON.stringify(randomData);
 let nameJson = 'random_' + Date.now();
 
-console.log(nameJson, randomJson);
-console.log(__dirname);
-
 fs.writeFileSync(`${__dirname}/${nameJson}.json`, randomJson);
