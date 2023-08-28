ee1d3436ed23064aecbecb08ebffbcc0aa254619
CarlosMunozDiaz <carlos10md@gmail.com>
2023-08-28 16:51:23 +0200

    .

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ let randomJson = JSON.stringify(randomData);
 let nameJson = 'random_' + Date.now();
 
 //fs.writeFile(`${nameJson}.json`, randomJson);
-fs.writeFile("data.json", randomJson, (err) => err && console.error(err));
+fs.writeFile("./data.json", randomJson, (err) => err && console.error(err));
+
+fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+if (err) throw err;
+console.log('Saved!');
+});
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
